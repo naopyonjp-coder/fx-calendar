7461d286e0270d2eba4f354647ff56512e577d83
naopyonjp-coder <naopyon.jp@gmail.com>
2026-06-06 09:12:13 +0900

    Move yearly total into data panel

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'fx-income-calendar-v8';
+const CACHE_NAME = 'fx-income-calendar-v9';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=8',
-  './app.js?v=8',
+  './styles.css?v=9',
+  './app.js?v=9',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
